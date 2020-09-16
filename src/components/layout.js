f1d4f6791efe407dde95a58caea57b02a9d14580
Alexander Fountain <alexander@digett.com>
2020-09-15 21:31:34 -0500

    build fix window defined

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,35 +5,37 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-import Header from "../components/regions/header"
-import HeaderCart from "../components/regions/headerCart"
-import Footer from "../components/regions/footer"
-import CartOverview from "../components/CartOverview"
-import { loadStripe } from "@stripe/stripe-js"
-import { CartProvider } from "use-shopping-cart"
-import { useShoppingCart, formatCurrencyString } from "use-shopping-cart"
-import "../components/scss/layout/layout.scss"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+import Header from "../components/regions/header";
+import HeaderCart from "../components/regions/headerCart";
+import Footer from "../components/regions/footer";
+import CartOverview from "../components/CartOverview";
+import { loadStripe } from "@stripe/stripe-js";
+import { CartProvider } from "use-shopping-cart";
+import { useShoppingCart, formatCurrencyString } from "use-shopping-cart";
+import "../components/scss/layout/layout.scss";
 
-const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
+const stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY);
 
 const Layout = ({ children, slug }) => {
-  console.log(slug)
   if (slug) {
-    var pageId = slug
+    var pageId = slug;
   } else {
-    var pageId = ""
+    var pageId = "";
   }
-
+  var currentWindow = "";
+  if (typeof window !== "undefined" && window) {
+    currentWindow = window.location.origin;
+  }
   return (
     <div id={pageId}>
       <CartProvider
         mode="client-only"
         stripe={stripePromise}
-        successUrl={`${window.location.origin}/page-2/`}
-        cancelUrl={`${window.location.origin}/`}
+        successUrl={`${currentWindow}/page-2/`}
+        cancelUrl={`${currentWindow}/`}
         currency="USD"
         allowedCountries={["US", "GB", "CA"]}
         billingAddressCollection={true}
@@ -44,11 +46,11 @@ const Layout = ({ children, slug }) => {
         <Footer />
       </CartProvider>
     </div>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
